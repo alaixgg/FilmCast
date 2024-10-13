6917b754a6c67aad5661da4ad85aa632fc7694c5
Ludwig <theludway@gmail.com>
2024-10-13 12:46:33 -0500

    Cláusula Licencia en Functions.py

--- Mineria de Datos/Analytics/Functions.py
+++ Mineria de Datos/Analytics/Functions.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+"""
+Casting Inteligente: Algoritmos para la selección óptima de actores
+
+Copyright (C) 2024  Alvarado Ludwig
+
+Este archivo es parte de FilmCast.
+
+FilmCast es software libre: puedes redistribuirlo y/o modificarlo bajo los términos de la Licencia Pública General de GNU tal como fue publicada por la Free Software Foundation, ya sea la versión 3 de la Licencia o cualquier versión posterior.
+
+FilmCast se distribuye con la esperanza de que sea útil, pero SIN NINGUNA GARANTÍA; ni siquiera la garantía implícita de COMERCIABILIDAD o IDONEIDAD PARA UN PROPÓSITO PARTICULAR. Consulta la Licencia Pública General de GNU para más detalles.
+
+Deberías haber recibido una copia de la Licencia Pública General de GNU junto con FilmCast. Si no, consulta https://www.gnu.org/licenses/.
+"""
 
 import matplotlib.pyplot as plt
 import pandas as pd
