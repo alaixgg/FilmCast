c1c263eb6980d78b1f5a74767c9cc0698ddbfb86
Ludwig <theludway@gmail.com>
2024-10-16 21:12:29 -0500

    Cosas

--- Mineria de Datos/Model/Distancia.py
+++ Mineria de Datos/Model/Distancia.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Casting Inteligente: Algoritmos para la selección óptima de actores
 
@@ -73,7 +74,7 @@ def find_nearest_neighbors_by_ranges(X, vector_input_ranges, weights, k=5):
 @app.route('/nearest-records', methods=['GET', 'POST'])
 def nearest_records():
     try:
-        data = request.jso
+        data = request.json
 
         vector_input_ranges = data['ranges']
         weights = data['weights']
