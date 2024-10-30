977c61da9745a88241314c9174b48f9965475d2d
alaixgg <andresj.alaixp@utadeo.edu.co>
2024-10-30 17:18:49 -0500

    Navegacion

--- Aplicaciones de Moviles/app/app/src/main/java/com/example/filmcast/ui/login/GuardadosActivity.kt
+++ Aplicaciones de Moviles/app/app/src/main/java/com/example/filmcast/ui/login/GuardadosActivity.kt
@@ -3,15 +3,13 @@ package com.example.filmcast.ui.login
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.filmcast.R
 
 class GuardadosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_guardados)
+        setContentView(R.layout.activity_actores_lista)
 
         }
     }
