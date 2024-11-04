bb5a2c5cb96af305f1677f3df2a99a63c782d2da
alaixgg <andresj.alaixp@utadeo.edu.co>
2024-11-03 20:22:53 -0500

    Correciones Crear_pryecto y MainActivity

--- Aplicaciones de Moviles/app/app/src/main/java/com/example/filmcast/ui/login/Crea_proyecto.kt
+++ Aplicaciones de Moviles/app/app/src/main/java/com/example/filmcast/ui/login/Crea_proyecto.kt
@@ -1,8 +1,11 @@
 package com.example.filmcast.ui.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.filmcast.R
 import com.example.filmcast.data.RetroFit.ApiService
 import com.example.filmcast.data.RetroFit.Proyecto
 import com.example.filmcast.databinding.ActivityCreaProyectoBinding
@@ -16,7 +19,7 @@ class Crea_proyecto : AppCompatActivity() {
     // Inicializa Retrofit y ApiService
     private val retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("https://api.example.com/") // Cambia esto a la URL de tu API
+            .baseUrl("https://api.example.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
@@ -30,35 +33,48 @@ class Crea_proyecto : AppCompatActivity() {
         binding = ActivityCreaProyectoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Configuración de listeners para los botones
         binding.ProGuardarProyecto.setOnClickListener {
-            val nombreProyecto = binding.ProNombreProyecto.text.toString()
-            val descripcionProyecto = binding.ProDescripcionProyecto.text.toString()
-            val generoCinematografico = binding.ProGeneroCinematografico.text.toString()
-            val fechaInicio = binding.ProFechaInicio.text.toString()
-            val fechaFin = binding.ProFechaFin.text.toString()
-            val presupuesto = binding.ProPresupuesto.text.toString().toDoubleOrNull()
+            guardarProyecto()
+        }
 
-            if (nombreProyecto.isNotEmpty() && descripcionProyecto.isNotEmpty() &&
-                generoCinematografico.isNotEmpty() && fechaInicio.isNotEmpty() &&
-                fechaFin.isNotEmpty() && presupuesto != null) {
+        val perfil = findViewById<ImageView>(R.id.BU_perfil)
+        perfil.setOnClickListener {
+            val intent = Intent(this, PerfilActivity::class.java)
+            startActivity(intent)
+        }
 
-                val proyecto = Proyecto(
-                    nombre = nombreProyecto,
-                    descripcion = descripcionProyecto,
-                    genero = generoCinematografico,
-                    fechaInicio = fechaInicio,
-                    fechaFin = fechaFin,
-                    presupuesto = presupuesto
-                )
-
-                
-            } else {
-                Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show()
-            }
+        val menu = findViewById<ImageView>(R.id.Bu_menu)
+        menu.setOnClickListener {
+            val intent = Intent(this, menuActivity::class.java)
+            startActivity(intent)
         }
     }
 
+    private fun guardarProyecto() {
+        val nombreProyecto = binding.ProNombreProyecto.text.toString()
+        val descripcionProyecto = binding.ProDescripcionProyecto.text.toString()
+        val generoCinematografico = binding.ProGeneroCinematografico.text.toString()
+        val fechaInicio = binding.ProFechaInicio.text.toString()
+        val fechaFin = binding.ProFechaFin.text.toString()
+        val presupuesto = binding.ProPresupuesto.text.toString().toDoubleOrNull()
 
+        if (nombreProyecto.isNotEmpty() && descripcionProyecto.isNotEmpty() &&
+            generoCinematografico.isNotEmpty() && fechaInicio.isNotEmpty() &&
+            fechaFin.isNotEmpty() && presupuesto != null) {
+
+            val proyecto = Proyecto(
+                nombre = nombreProyecto,
+                descripcion = descripcionProyecto,
+                genero = generoCinematografico,
+                fechaInicio = fechaInicio,
+                fechaFin = fechaFin,
+                presupuesto = presupuesto
+            )
+
+
+        } else {
+            Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show()
         }
-
-
+    }
+}
--- Aplicaciones de Moviles/app/app/src/main/java/com/example/filmcast/ui/login/MainActivity.kt
+++ Aplicaciones de Moviles/app/app/src/main/java/com/example/filmcast/ui/login/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.filmcast.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import android.content.Intent
 import com.example.filmcast.R
 
 class MainActivity : AppCompatActivity() {
