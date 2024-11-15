3cfc653a2f864b51cc86218fed2854fbcddb3e5b
alaixgg <andresj.alaixp@utadeo.edu.co>
2024-11-14 20:02:38 -0500

    SE MUESTRA LA INFORMACION EN LOS RECIYCLEVIEWS DE ResultadoActivity AAAAAAAA :DDDDDD

--- Aplicaciones de Moviles/app/app/src/main/java/com/example/filmcast/adapter/InfoPerfilAdapter.kt
+++ Aplicaciones de Moviles/app/app/src/main/java/com/example/filmcast/adapter/InfoPerfilAdapter.kt
@@ -7,17 +7,14 @@ import com.example.filmcast.R
 
 class InfoPerfilAdapter(private val infoActorList: List<InfoPerfil>) : RecyclerView.Adapter<InfoActorViewHolder>() {
 
-    // Se infla el layout de cada item y se crea el ViewHolder correspondiente
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InfoActorViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater.inflate(R.layout.item_perfil, parent, false)
         return InfoActorViewHolder(view)
     }
 
-    // Devolver el número de items en la lista
     override fun getItemCount(): Int = infoActorList.size
 
-    // Asignar los datos de cada item en el ViewHolder
     override fun onBindViewHolder(holder: InfoActorViewHolder, position: Int) {
         val item = infoActorList[position]
         holder.render(item)
